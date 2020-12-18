f71119cedfdfe2c5793984de9ba250dba3191e81
Gregory Grekov <d9fgrek@gmail.com>
2020-12-18 14:05:08 +0300

    Create section reviews

--- js/app.js
+++ js/app.js
@@ -11,3 +11,24 @@ headerClose.addEventListener('click', () => {
   nav.classList.toggle('active')
   body.classList.toggle('overflow--hidden')
 })
+
+const reviewsSlider = new Swiper('.reviews__container', {
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: '.reviews__pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      spaceBetween: 30,
+    },
+  },
+  autoplay: {
+    delay: 7000,
+  },
+})
+
+const reviewsContainer = document.querySelector('.reviews__container')
+reviewsContainer.addEventListener('mouseover', () => reviewsSlider.autoplay.stop())
+reviewsContainer.addEventListener('mouseout', () => reviewsSlider.autoplay.start())
