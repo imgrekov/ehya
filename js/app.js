b48441a9c20a17d365b734fd84be8b9d726d1c36
Gregory Grekov <d9fgrek@gmail.com>
2020-12-18 17:19:33 +0300

    Create section stories

--- js/app.js
+++ js/app.js
@@ -32,3 +32,22 @@ const reviewsSlider = new Swiper('.reviews__container', {
 const reviewsContainer = document.querySelector('.reviews__container')
 reviewsContainer.addEventListener('mouseover', () => reviewsSlider.autoplay.stop())
 reviewsContainer.addEventListener('mouseout', () => reviewsSlider.autoplay.start())
+
+const storiesSlider = new Swiper('.stories-slider', {
+  spaceBetween: 24,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  slidesPerColumn: 2,
+  navigation: {
+    prevEl: '.stories-slider__nav--prev',
+    nextEl: '.stories-slider__nav--next',
+  },
+  breakpoints: {
+    577: {
+      spaceBetween: 18,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      slidesPerColumn: 1,
+    },
+  },
+})
